bbeaab412501b0a2f32e2068c4441b88960037c1
Michael Pacak <vortexblaster09@gmail.com>
2026-09-24 19:35:11 -0600

    Fixing merge conflict

--- app/src/main/java/com/example/zephyrbbb/Shape.kt
+++ app/src/main/java/com/example/zephyrbbb/Shape.kt
@@ -1,5 +1,5 @@
 package com.example.zephyrbbb
 
-abstract class Shape(val x: Int, val y: Int, val color: String = "pink") {
+abstract class Shape(val x: Int, val y: Int, val color: String = "red") {
 
 }

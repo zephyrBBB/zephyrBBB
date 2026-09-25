dfb9a9cea6e50f521eb9743d5f0abc0ce088effd
Manroj Gill <manrojgill@hotmail.com>
2026-09-24 19:17:17 -0600

    Change Colour

--- app/src/main/java/com/example/zephyrbbb/Shape.kt
+++ app/src/main/java/com/example/zephyrbbb/Shape.kt
@@ -1,5 +1,5 @@
 package com.example.zephyrbbb
 
-abstract class Shape(val x: Int, val y: Int ,val color: String = "blue") {
+abstract class Shape(val x: Int, val y: Int ,val color: String = "pink") {
 
 }
